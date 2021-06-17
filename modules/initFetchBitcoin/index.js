e5c8beccbc8709cf231bcdd70ac27986c06b8c20
Andrew Adams <andreewb02@gmail.com>
2021-06-17 04:31:41 -0300

    fetchbitcoin refatorado

--- modules/initFetchBitcoin/index.js
+++ modules/initFetchBitcoin/index.js
@@ -1,8 +1,8 @@
-export function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export function initFetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      const btcPrice = document.querySelector('.btc-preco')
+      const btcPrice = document.querySelector(target)
 
       btcPrice.innerText = (1000 / json.BRL.sell).toFixed(4)
     }).catch((error) => {
