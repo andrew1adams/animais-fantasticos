bdcb1cfcd52618c8aaf57ef499f418b5c2dd1d07
Andrew Adams <andreewb02@gmail.com>
2021-06-18 01:50:28 -0300

    animascroll refeito

--- modules/initAnimationScroll/index.js
+++ modules/initAnimationScroll/index.js
@@ -1,26 +1,44 @@
 export default class AnimationScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.halfWindow = window.innerHeight * 0.7;
+    this.halfWindow = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this)
+    this.checkDistance = this.checkDistance.bind(this)
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const visibleSection = sectionTop - this.halfWindow < 0;
-
-      if (visibleSection) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  // pega a distância de cada elemento em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.halfWindow,
       }
-    });
+    })
+  }
+
+  // verifica a distância dos elementos em relação ao topo do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
+      }
+    })
   }
 
   init() {
-    this.animaScroll()
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance()
+      this.checkDistance()
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this
+  }
+
+  // remove o evento de scroll animation
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
